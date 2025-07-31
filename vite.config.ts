c53650f6aff4f59da0f0b620a0ab96ab80c20885
Yash Patel <yp752004@gmail.com>
2025-07-31 12:16:16 +0530

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-   base: "/yash-portfolio/",
+  //  base: "/yash-portfolio/",
   plugins: [
     react(),
     mode === 'development' &&
