baa4e1e1384f0536d48fc2e24946a6f65c33f3ca
NPatel10 <nayanpatel.np@yahoo.com>
2025-07-31 18:54:03 +0530

    Version Updated for all dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,11 +9,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  //  base: "/yash-portfolio/",
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    tailwindcss(),
   ].filter(Boolean),
   resolve: {
     alias: {
