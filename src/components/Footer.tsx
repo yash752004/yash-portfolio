19e631c2f6d406213a9a5e25cc9cd5c34a6450a4
NPatel10 <nayanpatel.np@yahoo.com>
2025-08-02 16:31:34 +0530

    COntact form update Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { motion } from "motion/react";
-import { Github, Linkedin, Heart, Mail, Phone } from "lucide-react";
+import { Github, Linkedin } from "lucide-react";
 
 const Footer = () => {
   var contactDetails = {
@@ -15,23 +15,23 @@ const Footer = () => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.6 }}
-      className="bg-card/50 border-t"
+      className="border-t"
     >
       <div className="container mx-auto px-6 py-8">
-        <div className="flex flex-col md:flex-row justify-between text-gray-600">
+        <div className="flex flex-col md:flex-row gap-8 justify-between text-gray-600">
           <div className="flex flex-col gap-4">
-            <div className="text-sm">Developed by</div>
+            <div className="text-sm text-monospace">// Developed by</div>
             <div className="flex flex-col">
-              <p className="text-xl font-semibold text-gray-950">{contactDetails.name}</p>
-              <p>{contactDetails.phone}</p>
-              <p>{contactDetails.email}</p>
+              <p className="text-2xl font-semibold text-gray-950">{contactDetails.name}</p>
+              <p className="text-monospace">{contactDetails.phone}</p>
+              <p className="text-monospace">{contactDetails.email}</p>
             </div>
             <div className="flex gap-4">
               <a href={contactDetails.github} target="_blank" className="hover:bg-gradient"><Github className="w-5 h-5" /></a>
               <a href={contactDetails.linkedin} target="_blank" className="hover:bg-gradient"><Linkedin className="w-5 h-5" /></a>
             </div>
           </div>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-0 md:gap-2">
               <a href={contactDetails.linkedin} target="_blank" className="hover:underline hover:text-gray-950">About</a>
               <a href={contactDetails.linkedin} target="_blank" className="hover:underline hover:text-gray-950">Portfolio</a>
               <a href={contactDetails.linkedin} target="_blank" className="hover:underline hover:text-gray-950">Contact</a>
